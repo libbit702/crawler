2129991ca3a4692562240330062013455b003c9f
lijunjun <lijunjun@idol001.com>
2017-11-30 11:07:24 +0800

    豆瓣豆列抓取

---
+++ class.doulist.php
@@ -0,0 +1,112 @@
+<?php
+
+require_once('class.base.php');
+
+class CrawlerDoulist extends CrawlerBase {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	/**
+	 * 网络IO，执行抓取
+	 */
+	public function doCrawl() {
+		if (!isset($this->crawl_config['ids'])) {
+			throw new InvalidArgumentException("doulist ids required for douban list");
+		}
+
+		if (empty($this->crawl_config['ids'])) {
+			throw new InvalidArgumentException("doulist ids cannot be empty for douban list");
+		}
+
+		$page = isset($this->crawl_config['page']) ? intval($this->crawl_config['page']) : 0;
+		if ($page <= 0) {
+			throw new InvalidArgumentException("invalid page setting for douban list");
+		}
+
+		$this->snoopy->agent = "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36";
+
+		foreach ($this->crawl_config['ids'] as $ei) {
+			for ($i=0; $i <= ($page-1); $i++) { 
+				$crawl_url = 'https://m.douban.com/doulist/'.$ei.'/?start='.$i * 25;
+
+				$this->log("开始请求地址:$crawl_url");
+
+				$this->snoopy->fetch($crawl_url);
+				
+				if ($this->snoopy->results === null) {
+					continue;
+				}
+
+				$crawl_result = $this->snoopy->results;
+
+				// $this->log("请求返回结果:$crawl_result");
+
+				$content = str_get_html($crawl_result);
+				$nodes = $content->find('.doulist-items li a');
+
+				if (count($nodes) > 0) {
+					foreach ($nodes as $node) {
+						$rcc = array();
+						$rcc['link'] = 'https://m.douban.com' . $node->getAttribute('href');
+						$rcc['pics'] = array();
+						
+						$cover_image = $node->find('.cover img', 0);
+						if ($cover_image) {
+							$rcc['pics'][] = $cover_image->getAttribute('src');
+						}
+
+						$info = $node->find('.info', 0);
+						if ($info) {
+							$title_node = $info->find('.title', 0);
+							if ($title_node) {
+								$rcc['title'] = $title_node->innertext();
+							}
+
+							$rank_node = $info->find('.rating-stars', 0);
+							if ($rank_node) {
+								$rcc['rank'] = $rank_node->getAttribute('data-rating');
+							}
+
+							$meta_node = $info->find('.meta', 0);
+							if ($meta_node) {
+								$rcc['meta'] = trim($meta_node->innertext());
+							}
+
+							$recommend_node = $info->find('.recommend', 0);
+							if ($recommend_node) {
+								$rcc['recommend'] = $recommend_node->innertext();
+							}
+						}
+						
+						$this->crawl_messages[] = $rcc;
+					}
+				}
+			}
+		}
+	}
+
+	public function doKeywordCheck() {
+		//豆列数据不检测关键字
+	}
+
+	public function doPublicTimeCheck() {
+		//豆列数据不检测发布时间
+	}
+
+	public function doImageCheck() {
+		//豆列数据不检测图片
+	}
+
+	public function doVideoCheck() {
+		//豆列数据不检测视频
+	}
+
+	/**
+	 * 经过过滤后的数据，可以做后续处理
+	 */
+	public function doMessage() {
+		// print_r($this->crawl_messages);
+	}
+}
---
+++ tests/CrawlerDoulistTest.php
@@ -0,0 +1,92 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once(dirname(__FILE__) . "/../class.doulist.php");
+
+/**
+ * @covers CrawlerDoulist
+ */
+final class CrawlerDoulistTest extends TestCase
+{
+    public function testCanBeCreatedFromDoulist()
+    {
+        $this->assertInstanceOf(
+            CrawlerDoulist::class,
+            new CrawlerDoulist()
+        );
+    }
+
+    public function testIdsCannotBeNull()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("doulist ids required for douban list");
+
+        $crawler = new CrawlerDoulist();
+        $crawler->setConfig([
+         'ids' => null
+        ]);
+        $crawler->prepareCrawl();
+        $crawler->executeCrawl();
+    }
+
+    public function testIdsCannotBeEmpty()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("doulist ids cannot be empty for douban list");
+
+        $crawler = new CrawlerDoulist();
+        $crawler->setConfig([
+            'ids' => []
+        ]);
+        $crawler->prepareCrawl();
+        $crawler->executeCrawl();
+    }
+
+    public function testPageCannotBeZero()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("invalid page setting for douban list");
+
+        $crawler = new CrawlerDoulist();
+        $crawler->setConfig([
+            'ids' => ['2943106'],
+            'page' => 0,
+        ]);
+        $crawler->prepareCrawl();
+        $crawler->executeCrawl();
+    }
+
+    public function testPageCannotBeNegative()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("invalid page setting for douban list");
+
+        $crawler = new CrawlerDoulist();
+        $crawler->setConfig([
+            'ids' => ['2943106'],
+            'page' => -1,
+        ]);
+        $crawler->prepareCrawl();
+        $crawler->executeCrawl();
+    }
+
+    public function testCanGetEnoughMessage()
+    {
+        $crawler = new CrawlerDoulist();
+        $crawler->setConfig([
+            'ids' => ['2943106'],
+            'page' => 3,
+        ]);
+        $crawler->prepareCrawl();
+        $crawler->executeCrawl();
+
+        $this->assertGreaterThanOrEqual(75,count($crawler->getMessage()));
+        foreach ($crawler->getMessage() as $key => $value) {
+            $this->assertArrayHasKey('meta',$value);
+            $this->assertArrayHasKey('link',$value);
+            $this->assertArrayHasKey('title',$value);
+            $this->assertArrayHasKey('rank',$value);
+        }
+    }
+}
